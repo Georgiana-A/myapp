5744085b61c28049f5cbd3b79b4f1d15ab4e5ee3
Georgiana Apristavitei <g.apristavitei@gmail.com>
2017-08-11 11:56:15 +0300

    first commit

---
+++ Angular4/router-samples/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-root',
+  template: `
+        <a [routerLink]="['/']">Home</a>
+        <a [routerLink]="['/product']">Product</a>
+        <a [routerLink]="['/product', 23434]">Product Param</a>
+        <router-outlet></router-outlet>
+    `
+})
+export class AppComponent {}
